64e9d33087aeadd6966d19f35beea653f08ae861
DONATUS ANKAH <ankahdonatus@gmail.com>
2023-09-04 18:52:33 +0000

    Update get-agent-data.js

--- js/get-agent-data.js
+++ js/get-agent-data.js
@@ -116,6 +116,115 @@ function disableButton(state) {
 }
 
 //fetch agent, token and candidate info on dashboard load 
+// function readData(){
+
+//     //checkTokenStatus();
+//     countTokens();
+//     //getTokenIdFromFirebase();
+
+//     firebase.auth().onAuthStateChanged(function(user) {
+//         if (user) {
+//           // User is signed in.
+
+//           var agentID = user.uid;
+//           console.log("AgentID: " + agentID);
+
+//           var tbody = document.querySelector("#data-table tbody");
+//           var rbody = document.querySelector("#data-mobile tbody");
+//           tbody.innerHTML = "";
+//           rbody.innerHTML = "";
+
+//             var agentCollectionId = agentID;
+
+//             var link = "";
+//             var token = "";
+//             var tokenId = "";
+//             var tokenStatus = "";
+//             var candidateName = "";
+
+//             // Reference to the subcollection
+//           var tokencollectionRef = firebase.database().ref('agents/' + agentCollectionId + '/tokens');
+//           var tokencollectionRefm = firebase.database().ref('agents/' + agentCollectionId + '/tokens');
+
+//             // Retrieve the subcollection data
+//             tokencollectionRef.once('value')
+//             .then(function(snapshot) {
+
+//                 snapshot.forEach(function(childSnapshot){
+//                     var tokenData = childSnapshot.val();
+
+//                     var row = document.createElement("tr");
+
+//                     // Create table data cells and populate with data
+//                     tokenId = document.createElement("td");
+//                     tokenId.textContent = childSnapshot.key;
+//                     row.appendChild(tokenId);
+
+//                     token = document.createElement("td");
+//                     token.textContent = tokenData.token;
+//                     row.appendChild(token);
+
+//                     link = document.createElement("td"); 
+//                     link.textContent = tokenData.linkWithToken  + "?agentID=" + agentID + "&" + "tokenID=" + childSnapshot.key;
+//                     link.setAttribute("class", "hide-column");       
+//                     row.appendChild(link);
+
+//                     candidateName =  document.createElement("td");
+//                     candidateName.textContent = tokenData.candidateName;
+//                     row.appendChild(candidateName);
+
+//                     linkStatus =  document.createElement("td");
+//                     linkStatus.textContent = tokenData.linkStatus;
+//                     row.appendChild(linkStatus);
+
+//                     tokenStatus = document.createElement("td");
+//                     tokenStatus.textContent = tokenData.tokenStatus;
+//                     row.appendChild(tokenStatus);
+
+//                     // Append the row to the table body
+//                     tbody.appendChild(row);
+
+//                 })
+//             })
+//             .catch(function(error) {
+//                 console.error('Error retrieving subcollection data:', error);
+//             });
+          
+          
+//         tokencollectionRefm.once('value')
+//             .then(function(snapshot) {
+
+//                 snapshot.forEach(function(childSnapshot){
+//                     var tokenData = childSnapshot.val();
+
+//                     var row = document.createElement("tr");
+
+//                     // Create table data cells and populate with data
+//                     token = document.createElement("td");
+//                     token.textContent = tokenData.token;
+//                     row.appendChild(token);
+
+//                     candidateName =  document.createElement("td");
+//                     candidateName.textContent = tokenData.candidateName;
+//                     row.appendChild(candidateName);
+                  
+//                     tokenStatus = document.createElement("td");
+//                     tokenStatus.textContent = tokenData.tokenStatus;
+//                     row.appendChild(tokenStatus);
+
+//                     // Append the row to the table body
+//                     rbody.appendChild(row);
+
+//                 })
+//             })
+//             .catch(function(error) {
+//                 console.error('Error retrieving subcollection data:', error);
+//             });
+//         }
+//       });
+       
+// }
+
 function readData(){
 
     //checkTokenStatus();
@@ -130,9 +239,7 @@ function readData(){
           console.log("AgentID: " + agentID);
 
           var tbody = document.querySelector("#data-table tbody");
-          var rbody = document.querySelector("#data-mobile tbody");
           tbody.innerHTML = "";
-          rbody.innerHTML = "";
 
             var agentCollectionId = agentID;
 
@@ -141,10 +248,10 @@ function readData(){
             var tokenId = "";
             var tokenStatus = "";
             var candidateName = "";
+            var createdAt = "";
 
             // Reference to the subcollection
-          var tokencollectionRef = firebase.database().ref('agents/' + agentCollectionId + '/tokens');
-          var tokencollectionRefm = firebase.database().ref('agents/' + agentCollectionId + '/tokens');
+            var tokencollectionRef = firebase.database().ref('agents/' + agentCollectionId + '/tokens');
 
             // Retrieve the subcollection data
             tokencollectionRef.once('value')
@@ -181,39 +288,12 @@ function readData(){
                     tokenStatus.textContent = tokenData.tokenStatus;
                     row.appendChild(tokenStatus);
 
+                    createdAt = document.createElement("td");
+                    createdAt.textContent = tokenData.createdAt;
+                    row.appendChild(createdAt);
+
                     // Append the row to the table body
                     tbody.appendChild(row);
-
-                })
-            })
-            .catch(function(error) {
-                console.error('Error retrieving subcollection data:', error);
-            });
-          
-          
-        tokencollectionRefm.once('value')
-            .then(function(snapshot) {
-
-                snapshot.forEach(function(childSnapshot){
-                    var tokenData = childSnapshot.val();
-
-                    var row = document.createElement("tr");
-
-                    // Create table data cells and populate with data
-                    token = document.createElement("td");
-                    token.textContent = tokenData.token;
-                    row.appendChild(token);
-
-                    candidateName =  document.createElement("td");
-                    candidateName.textContent = tokenData.candidateName;
-                    row.appendChild(candidateName);
-                  
-                    tokenStatus = document.createElement("td");
-                    tokenStatus.textContent = tokenData.tokenStatus;
-                    row.appendChild(tokenStatus);
-
-                    // Append the row to the table body
-                    rbody.appendChild(row);
 
                 })
             })
